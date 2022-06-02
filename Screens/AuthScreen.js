9a2d1b80640508b07c5b57398fa31247cb267c94
omaima-9 <satsicko@gmail.com>
2022-06-02 22:27:50 +0200

    AuthScreen design

--- Screens/AuthScreen.js
+++ Screens/AuthScreen.js
@@ -1,10 +1,18 @@
 // expo install expo-web-browser expo-auth-session expo-random
 
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View, Text, Image, Button } from 'react-native';
-import * as Google from 'expo-auth-session/providers/google';
-import * as WebBrowser from 'expo-web-browser';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  Button,
+} from "react-native";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
+import AwesomeButton from "react-native-really-awesome-button";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -16,7 +24,7 @@ export default function AuthScreen() {
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: "",
     iosClientId: "",
-    expoClientId: ""
+    expoClientId: "",
   });
 
   React.useEffect(() => {
@@ -27,11 +35,14 @@ export default function AuthScreen() {
   }, [response]);
 
   async function getUserData() {
-    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: { Authorization: `Bearer ${accessToken}`}
-    });
+    let userInfoResponse = await fetch(
+      "https://www.googleapis.com/userinfo/v2/me",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
 
-    userInfoResponse.json().then(data => {
+    userInfoResponse.json().then((data) => {
       setUserInfo(data);
     });
   }
@@ -40,7 +51,7 @@ export default function AuthScreen() {
     if (userInfo) {
       return (
         <View style={styles.userInfo}>
-          <Image source={{uri: userInfo.picture}} style={styles.profilePic} />
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
           <Text>Welcome {userInfo.name}</Text>
           <Text>{userInfo.email}</Text>
         </View>
@@ -50,11 +61,54 @@ export default function AuthScreen() {
 
   return (
     <View style={styles.container}>
-      {showUserInfo()}
-      <Button 
-        title={accessToken ? "Get User Data" : "Login"}
-        onPress={accessToken ? getUserData : () => { promptAsync({useProxy: false, showInRecents: true}) }}
+      <View style={{marginBottom:100 }}>
+      <Image
+        style={{ height: 100, width: 100 }}
+        source={require("../assets/logo.png")}
       />
+      </View>
+     
+      {showUserInfo()}
+      <View style={{ flexDirection: "row"}}>
+      
+        <View style={styles.divider} />
+        <Text style={{ fontWeight: "500", color: "#284B63" }}>
+          {" "}
+          WELCOME TO <Text style={styles.title}>ORGANIZELY</Text>
+        </Text>
+
+        <View style={styles.divider} />
+      </View>
+      <Text>
+        {"\n "}
+        {"\n "}
+        {"\n "}
+        {"\n "}
+        {"\n "}
+        {"\n "}
+        {"\n "}
+      </Text>
+      <TouchableOpacity>
+        <AwesomeButton
+          backgroundColor="#333533"
+          width={150}
+          onPress={
+            accessToken
+              ? getUserData
+              : () => {
+                  promptAsync({ useProxy: false, showInRecents: true });
+                }
+          }
+        >
+          <Text style={{ color: "#fff" }}>
+            {accessToken ? "Get User Data" : "Sign in by Google"}{" "}
+          </Text>
+          <Image
+            style={{ height: 20, width: 20 }}
+            source={require("../assets/google.png")}
+          />
+        </AwesomeButton>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -63,16 +117,29 @@ export default function AuthScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EDDF",
+    alignItems: "center",
+    // justifyContent: "center",
   },
   userInfo: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   profilePic: {
     width: 50,
-    height: 50
-  }
+    height: 50,
+  },
+  divider: {
+    backgroundColor: "#284B63",
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+    paddingVertical: 3,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: "#F5CB5C",
+    paddingHorizontal: 64,
+  },
 });
