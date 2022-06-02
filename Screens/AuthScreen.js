45e50e50686fac64ac424c48dc3a372399e34bc8
omaima-9 <satsicko@gmail.com>
2022-06-02 23:34:13 +0200

    Update Auth

--- Screens/AuthScreen.js
+++ Screens/AuthScreen.js
@@ -1,12 +1,18 @@
 // expo install expo-web-browser expo-auth-session expo-random
 
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import * as Google from 'expo-auth-session/providers/google';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+  Image,
+  Button,
+} from "react-native";
+import * as Google from "expo-auth-session/providers/google";
+import * as WebBrowser from "expo-web-browser";
 import AwesomeButton from "react-native-really-awesome-button";
-
-import * as WebBrowser from 'expo-web-browser';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -16,9 +22,9 @@ export default function AuthScreen() {
   const [message, setMessage] = React.useState();
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: "242080826208-2d634c5hkngg8n3ig0iv6guir7jdmmfu.apps.googleusercontent.com",
-    iosClientId: "242080826208-riitrtfpcj94sd3cd99dgfqqvp2v143o.apps.googleusercontent.com",
-    expoClientId: "242080826208-67fhgt1dqvb9lo4jfgpt006jk8k4ovsh.apps.googleusercontent.com"
+    androidClientId: "",
+    iosClientId: "",
+    expoClientId: "",
   });
 
   React.useEffect(() => {
@@ -29,11 +35,14 @@ export default function AuthScreen() {
   }, [response]);
 
   async function getUserData() {
-    let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: { Authorization: `Bearer ${accessToken}`}
-    });
+    let userInfoResponse = await fetch(
+      "https://www.googleapis.com/userinfo/v2/me",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
 
-    userInfoResponse.json().then(data => {
+    userInfoResponse.json().then((data) => {
       setUserInfo(data);
     });
   }
@@ -42,7 +51,7 @@ export default function AuthScreen() {
     if (userInfo) {
       return (
         <View style={styles.userInfo}>
-          <Image source={{uri: userInfo.picture}} style={styles.profilePic} />
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
           <Text>Welcome {userInfo.name}</Text>
           <Text>{userInfo.email}</Text>
         </View>
@@ -52,6 +61,13 @@ export default function AuthScreen() {
 
   return (
     <View style={styles.container}>
+      <View style={{marginBottom:100 }}>
+      <Image
+        style={{ height: 100, width: 100 }}
+        source={require("../assets/logo.png")}
+      />
+      </View>
+     
       {showUserInfo()}
       <View style={{ flexDirection: "row"}}>
       
@@ -84,7 +100,6 @@ export default function AuthScreen() {
                 }
           }
         >
-          
           <Text style={{ color: "#fff" }}>
             {accessToken ? "Get User Data" : "Sign in by Google"}{" "}
           </Text>
@@ -102,16 +117,29 @@ export default function AuthScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EDDF",
+    alignItems: "center",
+    // justifyContent: "center",
   },
   userInfo: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   profilePic: {
     width: 50,
-    height: 50
-  }
+    height: 50,
+  },
+  divider: {
+    backgroundColor: "#284B63",
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+    paddingVertical: 3,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: "#F5CB5C",
+    paddingHorizontal: 64,
+  },
 });
